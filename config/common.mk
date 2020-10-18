22ded17fef94ae3b32679624919ccd2d6212bcd9
NurKeinNeid <mralexman3000@gmail.com>
2020-10-18 03:05:09 +0200

    pa: Add prebuilt keyboard swipe lib google version
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/pa/prebuilt/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
         vendor/pa/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
+# Extra packages
+PRODUCT_PACKAGES += \
+    libjni_latinimegoogle
+
 # GApps
 ifneq ($(TARGET_DISABLES_GAPPS), true)
 
